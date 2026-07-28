a92311fa11f966cce7f618012b0930fb601c667c
Jochum van der Ploeg <jochum@vdploeg.net>
2026-07-28 09:39:58 +0200

    feat: add `greater/less` than or equals

--- packages/raindrop/lib/src/definitions/column_types/text.dart
+++ packages/raindrop/lib/src/definitions/column_types/text.dart
@@ -3,4 +3,18 @@ import 'package:raindrop/raindrop.dart';
 extension StringOperators<V extends String?> on ColumnOf<V> {
   /// String like [value].
   SQL like(ColumnOr<String> value) => SQL([this, const RawSQL('LIKE'), value]);
+
+  /// Lexicographic ordering.
+  SQL greaterThan(ColumnOr<String> value) => SQL([this, Op.greaterThan, value]);
+
+  /// Lexicographic ordering.
+  SQL greaterThanOrEqual(ColumnOr<String> value) =>
+      SQL([this, Op.greaterThanOrEqual, value]);
+
+  /// Lexicographic ordering.
+  SQL lessThan(ColumnOr<String> value) => SQL([this, Op.lessThan, value]);
+
+  /// Lexicographic ordering.
+  SQL lessThanOrEqual(ColumnOr<String> value) =>
+      SQL([this, Op.lessThanOrEqual, value]);
 }
